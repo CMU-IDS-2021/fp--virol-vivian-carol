ff2da581fa4bb97b64d5275d89d88889350da650
Carol Ho <car8121@gmail.com>
2021-05-01 13:56:41 -0400

    layout adjustment: using expandable components and add description

--- app_carol.py
+++ app_carol.py
@@ -6,52 +6,81 @@ import requests
 from utils import *
 
 
-def main():
+def without_result():
+  pass
+  st.markdown("We couldnt find the result!")
 
-  data = get_api_data("Ang Lee")
-  bio = data['biography']
+def with_result(data):
+#biography
+  name = data['name']
+  st.markdown('# '+name)
+  col1, col2, col3, col4 = st.beta_columns((1,1,1,1))
+  birthday = data['birthday']
+  col1.markdown('**Birthday**')
+  col1.markdown(birthday)
+  place_of_birth = data['place_of_birth']
+  col2.markdown('**Place of Birth**')
+  col2.markdown(place_of_birth)
+  popularity = data['popularity']
+  col3.markdown('**Popularity**')
+  col3.markdown(popularity)
+  col4.markdown('**Number of works**')
+  col4.markdown('23')#need to calculate  
+  with st.beta_expander("Show me the biography!"):
+    col1, col2 = st.beta_columns((1,2))
+    bio = data['biography']
+    pic_url = 'https://image.tmdb.org/t/p/w300/'
+    profile_path = data['profile_path']
+    profile_path = pic_url + profile_path
+    col1.image(profile_path)
+    col2.markdown(bio)
+
   works = data['works']['cast']
+
   #name = data['works']['cast']
   df=pd.DataFrame(works)
-  st.dataframe(df)
-  #casts = data['cast']
-  st.markdown(bio)
 
-  #st.markdown(works)
-  #st.markdown(name)
+  #data preperation - line chart
+  df.loc[:, 'imdb_id'] = df.id.apply(lambda x:handle_get_moviedb_imdb_id(x))
+  new_arr = []
+  for index, row in df.iterrows():
+    imdb_id = row['imdb_id']
+    if imdb_id:
+      res = get_OMDB_data(imdb_id)
+      row['imdbRating'] = res.get('imdbRating', 'N/A')
+      row['BoxOffice'] = res.get('BoxOffice', 'N/A')
+      row['Year'] = res.get('Year', 'N/A')
 
-  #themoviedb API - search for movie by id
-  response = requests.get("https://api.themoviedb.org/3/movie/310674?api_key=8bab42520fb79424d47245ab1e5406cf&language=en-US")
-  r = response.json()
-  #df=pd.DataFrame(r['cast'])
-  st.markdown(r)
-  st.dataframe(df)
+      res = handle_get_moviedb_movie_detail(row['id'])
+      if res:
+        row['revenue'] = res.get('revenue', 0)
+        row['runtime'] = res.get('runtime', 0)
+        row['budget'] = res.get('budget', 0)
 
-  #OMDB API
-  response = requests.get("http://www.omdbapi.com/?i=tt2294629&apikey=e6dd17dc")
-  st.markdown(response.status_code)
-  st.markdown(response.content)
-  #sidebar_text
-  add_text = st.sidebar.markdown("Which film producer/actor are you exploring?")
-  add_textinput = st.sidebar.text_input(
-    "such as Ang Lee",
-  )
+      new_arr.append(row)
 
-  #sidebar_slider
-  add_text = st.sidebar.markdown("Which year range of works are you exploring?")  
-  add_slider = st.sidebar.slider(
-    'the data on the chart will change base on the range.',1900,2021,(1900,2021)
-    )
-  #sidebar_checkbox
-  add_text = st.sidebar.markdown("Line Chart - What’s the imdb Rating by work")
-  add_checkbox = st.sidebar.checkbox("budget")
-  add_checkbox = st.sidebar.checkbox("revenue")
-  add_text = st.sidebar.markdown("Bar Chart - How will the attribute distribute by origin and gender?")
-  add_dropdown = st.sidebar.selectbox("select the attribute below",("imdb Rating","revenue","budget"))
+  related_movies_df = pd.DataFrame(new_arr)
+  revenue_mean = related_movies_df[related_movies_df.revenue > 0].revenue.mean()
+  box_office_mean = related_movies_df[related_movies_df.BoxOffice != 'N/A'].BoxOffice.str.replace('$', '').str.replace(',', '').astype(float, errors='ignore').mean()
+  imdb_rate_mean = related_movies_df[related_movies_df.imdbRating != 'N/A'].imdbRating.astype(float, errors='ignore').mean()
+  budget_mean = related_movies_df[related_movies_df.budget > 0].budget.mean()
+  imdb_rate_mean = round(imdb_rate_mean, 2)
+  budget_mean = round(budget_mean,2)
+  box_office_mean = round(box_office_mean,2)
+  revenue_mean = round(revenue_mean,2)
+  st.markdown('### Average numbers of the works:')
+  col1, col2, col3, col4 = st.beta_columns((1,1,1,1))
+  col1.markdown('**Avg Revenue**')
+  col1.markdown(f'{revenue_mean}')
+  col2.markdown('**Avg Budget**')
+  col2.markdown(f'{budget_mean}')
+  col3.markdown('**Avg Box Office**')
+  col3.markdown(f'{box_office_mean}')
+  col4.markdown('**Avg IMDB Rate**')
+  col4.markdown(f'{imdb_rate_mean}')#need to calculate 
+ 
 
-  selector = alt.selection_single(empty='all', fields=['Count'])    
-
-  #data preparation- genres count
+ #data preparation- genres count
   movie_genre_ids = df[~df.title.isnull()][['title', 'genre_ids']].to_dict(orient ='records')
 
   movie_genre = []
@@ -63,13 +92,22 @@ def main():
       movie_genre.append({'movie': x['title'], 'genres': genre_name})
 
   movie_genre_df = pd.DataFrame(movie_genre)
-  movie_genre_summary = movie_genre_df.groupby(['genres']).agg({'movie': ['count' , pd.unique]}).reset_index()
+  #st.markdown(f'movie_genre_df shape: {movie_genre_df.shape}')
+  movie_genre_summary = movie_genre_df.groupby(['genres']).agg({'movie': ['count' , 'unique']}).reset_index()
   movie_genre_summary.columns = ['Genres', 'Count', 'Movie']
   movie_genre_summary.loc[:, 'Movie'] = movie_genre_summary.Movie.apply(lambda x: x if isinstance(x, str) else ', '.join(x.tolist()))
+ 
+  #bar chart-description
+  st.header("Bar Chart - Genres/Counts")
+  st.subheader(":clapper: What genres are the works in?")
+  st.markdown("Instructions: Hover on the bar to see the list of the movies.")
+  with st.beta_expander("See the dataframe."):
+    st.dataframe(movie_genre_df)
+ 
   
   #bar chart-genres count
   df_bar=movie_genre_summary.copy()
-  st.dataframe(df)
+  #st.dataframe(df)
   bar_genres = alt.Chart(df_bar).mark_bar(color='#8624F5', opacity=0.8, thickness=5).encode(
     alt.X('Genres:O',
       sort={"field": "Genres", "order": "ascending"},
@@ -89,23 +127,14 @@ def main():
 
   st.write(bar_genres)
 
-  #data preperation - line
-  def handle_get_moviedb_imdb_id(movie_id):
-    try:
-      result = get_moviedb_imdb_id(movie_id)
-      return result
-    except:
-      pass
-  df.loc[:, 'imdb_id'] = df.id.apply(lambda x:handle_get_moviedb_imdb_id(x))
-  new_arr = []
-  for index, row in df.iterrows():
-    imdb_id = row['imdb_id']
-    if imdb_id:
-       res = get_OMDB_data(imdb_id)
-       row['imdbRating'] = res['imdbRating']
-       row['BoxOffice'] = res['BoxOffice']
-       row['Year'] = res['Year']
-       new_arr.append(row)
+  #line chart-description
+  st.header("Line Chart - IMDB Rating v.s BoxOffice/Budget/Revenue")
+  st.subheader(":clapper: What genres are the works in?")
+  st.markdown("Instructions: Hover on the bar to see the list of the movies.")
+  with st.beta_expander("See the dataframe."):
+    st.dataframe(movie_genre_df)
+
+
   #line chart
   # Create a selection that chooses the nearest point & selects based on x-value
   
@@ -169,10 +198,6 @@ def main():
   st.write(chart)
   #avg bar chart
   st.markdown("<div id='comparison'>", unsafe_allow_html=True)
-  st.header("Bar Chart - David Bowie's albums with average features")
-  st.subheader(":musical_note: How do the acoustic features of Bowie's albums differ from other songs in that decade?")
-  st.markdown("Instructions: Click on the checkbox to compare with other songs from that decade. Click on the bar to highlight.")
-  
   test2 = {'name':['AAA','BBB','CCC','DDD'],'type':['director','director','actor','actor'],'gender':['1','1','2','2'],'origin':['Asia','U.S','U.S','UK'],'avg budget':['19999','19999','19999','19999'],'avg box-office':['19999','19999','19999','19999'],'avg revenue':['19999','19999','19999','19999']}
   df=pd.DataFrame(test2)
   st.dataframe(df)
@@ -205,5 +230,20 @@ def main():
   st.write(bar_avg+rule)
 
 
+def main():
 
-main()
+  st.markdown('# :popcorn:Film Producer/Actor Data Jam!')
+  st.markdown('This platform allows you to explore the film producer/actor by their works.')
+  
+  add_textinput = st.text_input("Search for film producer/actor:", "Ang Lee")
+  
+  data = get_api_data(add_textinput)
+  print(data)
+  if data and data.get('works', {}).get('cast'):
+    with_result(data)
+  else:
+    without_result()
+
+
+if __name__ == '__main__':
+  main()
